4ea8d6f6cd9aaf9bd35ee95d4353a6eda2a87563
aalaebl <aalaeedine2000@outlook.fr>
2023-08-08 23:23:37 +0100

    Update SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import UrlIcon from "~/components/UrlIcon";
 import { api } from "~/utils/api";
 import createURL from "~/utils/createURL";
 
+import thumbnail from "~/public/thumbnail.jpg";
+
 export default function Home() {
   // const { data: urls, isLoading } = api.url.getAll.useQuery();
   const [generatedURL, setGeneratedURL] = React.useState<string>("");
@@ -40,6 +42,45 @@ export default function Home() {
           name="description"
           content="SnapURL is a powerful URL shortener app that helps you simplify long web addresses into concise and shareable links. With built-in analytics, track the number of clicks, IP addresses, and timestamps for each link. SnapURL provides a seamless experience for transforming and managing your links. Start shortening and sharing with ease today!"
         />
+
+        <meta key="og_type" property="og:type" content="website" />
+        <meta key="og_title" property="og:title" content="SnapURL" />
+        <meta
+          key="og_description"
+          property="og:description"
+          content="SnapURL is a powerful URL shortener app that helps you simplify long web addresses into concise and shareable links. With built-in analytics, track the number of clicks, IP addresses, and timestamps for each link. SnapURL provides a seamless experience for transforming and managing your links. Start shortening and sharing with ease today!"
+        />
+        <meta key="og_locale" property="og:locale" content="en_IE" />
+        <meta key="og_site_name" property="og:site_name" content="SnapURL" />
+        <meta
+          key="og_url"
+          property="og:url"
+          content="https://snapurl.vercel.app/"
+        />
+        <meta key="og_site_name" property="og:site_name" content="SnapURL" />
+        <meta key="og_image" property="og:image" content={thumbnail.src} />
+        <meta key="og_image:alt" property="og:image:alt" content="SnapURL" />
+        <meta key="og_image:width" property="og:image:width" content="1200" />
+        <meta key="og_image:height" property="og:image:height" content="630" />
+
+        <meta name="robots" content="index,follow" />
+
+        <meta
+          key="twitter:card"
+          name="twitter:card"
+          content="summary_large_image"
+        />
+        <meta key="twitter:site" name="twitter:site" content="SnapURL" />
+        <meta key="twitter:creator" name="twitter:creator" content="SnapURL" />
+        <meta key="twitter:title" property="twitter:title" content="SnapURL" />
+        <meta
+          key="twitter:description"
+          property="twitter:description"
+          content="SnapURL is a powerful URL shortener app that helps you simplify long web addresses into concise and shareable links. With built-in analytics, track the number of clicks, IP addresses, and timestamps for each link. SnapURL provides a seamless experience for transforming and managing your links. Start shortening and sharing with ease today!"
+        />
+
+        <link rel="canonical" href="https://snapurl.vercel.app/" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen flex-col items-center justify-center gap-4 overflow-hidden bg-neutral-900 px-12 py-12">
