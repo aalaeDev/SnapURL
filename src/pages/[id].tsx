d3f8db78b635f116ae61b7d7ac8b359edf59a656
aalaebl <aalaeedine2000@outlook.fr>
2023-08-10 10:49:45 +0100

    Update Analytics

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,23 +1,22 @@
-import type { NextPage, GetServerSidePropsContext } from "next";
+import type { GetServerSidePropsContext } from "next";
 import superjson from "superjson";
 import { prisma } from "~/server/db";
 import { createServerSideHelpers } from "@trpc/react-query/server";
 import { appRouter } from "~/server/api/root";
 
-import { api } from "~/utils/api";
-
-import { useEffectOnce } from "~/hooks/useEffectOnce";
 import { type IPAddressInfo } from "~/types";
-import { useRouter } from "next/router";
 
 export async function getServerSideProps(
   context: GetServerSidePropsContext<{ id: string }>
 ) {
   const urlCode = context.params?.id;
 
-  const visitorIP =
-    context.req.headers["x-forwarded-for"] ??
-    context.req.connection.remoteAddress;
+  let visitorIP =
+    context.req.headers["x-forwarded-for"] ?? context.req.socket.remoteAddress;
+
+  if (typeof visitorIP !== "string") {
+    visitorIP = "0.0.0.0";
+  }
 
   const ssr = createServerSideHelpers({
     router: appRouter,
@@ -45,6 +44,31 @@ export async function getServerSideProps(
     };
   }
 
+  const visitor = await fetch(`http://ip-api.com/json/${visitorIP}`)
+    .then((response) => {
+      if (!response.ok) {
+        return undefined;
+      }
+      return response.json();
+    })
+    .then((data: IPAddressInfo) => {
+      return data;
+    })
+    .catch((_) => {
+      return undefined;
+    });
+
+  await prisma.visitor.create({
+    data: {
+      ip: visitor?.query,
+      city: visitor?.city,
+      country: visitor?.country,
+      region: visitor?.regionName,
+      isp: visitor?.isp,
+      urlId: url.id,
+    },
+  });
+
   await prisma.url.update({
     where: {
       id: url.id,
@@ -54,74 +78,14 @@ export async function getServerSideProps(
     },
   });
 
-  // return {
-  //   redirect: {
-  //     destination: url.originalUrl,
-  //     permanent: false,
-  //   },
-  // };
-
   return {
-    props: {
-      trpcState: ssr.dehydrate(),
-      urlId: url.id,
-      originalUrl: url.originalUrl,
-      visitorIP,
+    redirect: {
+      destination: url.originalUrl,
+      permanent: false,
     },
   };
 }
 
-const Redirect: NextPage<{
-  urlId: string;
-  originalUrl: string;
-  visitorIP: string | string[] | undefined;
-}> = ({ urlId, originalUrl, visitorIP }) => {
-  const router = useRouter();
-  const { mutate: createVisitor } = api.url.createVisitor.useMutation({
-    // onSuccess: () => {
-    //   void router.replace(originalUrl);
-    // },
-    // onError: () => {
-    //   void router.replace(originalUrl);
-    // },
-  });
-
-  async function getVisitorIp() {
-    const visitor = await fetch("http://ip-api.com/json/?fields=61439")
-      .then((response) => {
-        if (!response.ok) {
-          return undefined;
-        }
-        return response.json();
-      })
-      .then((data: IPAddressInfo) => {
-        return data;
-      })
-      .catch((_) => {
-        return undefined;
-      });
-
-    return visitor;
-  }
-
-  useEffectOnce(() => {
-    const generateAnalytics = async () => {
-      const visitor = await getVisitorIp();
-
-      createVisitor({
-        urlId,
-        ip: visitor?.query,
-        country: visitor?.country,
-        city: visitor?.city,
-        region: visitor?.regionName,
-        isp: visitor?.isp,
-      });
-    };
-
-    void generateAnalytics();
-  });
-
-  return <>{visitorIP}</>;
-};
-
-export default Redirect;
+export default function Redirect() {
+  return <div>Redirecting...</div>;
+}
